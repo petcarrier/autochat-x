8b0e0ed6dbecdd59f9909433bc4bd0e29ac35280
Sobte <i@sobte.me>
2025-05-14 12:41:46 +0800

    Initial commit

---
+++ develop/shells/default/default.nix
@@ -0,0 +1,28 @@
+{
+  inputs,
+  pkgs,
+  mkShell,
+  system,
+  ...
+}:
+let
+  commitCheck = inputs.self.checks.${system}.pre-commit-check.shellHook;
+  node = pkgs.nodejs;
+in
+mkShell {
+  packages = with pkgs; [
+    node2nix
+    node
+    corepack
+  ];
+
+  shellHook = ''
+    ${commitCheck}
+
+    # Disable download prompt for corepack
+    export COREPACK_ENABLE_DOWNLOAD_PROMPT=0
+    echo "node version: `${node}/bin/node --version`"
+  '';
+
+  buildInputs = inputs.self.checks.${system}.pre-commit-check.enabledPackages;
+}
